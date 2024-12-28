8f06fcc16e8b2fde15c3c37800baddbc4eee9ed8
Kai Kameyama <1203kk06@gmail.com>
2024-12-28 11:02:53 +0900

    completed all of the css html and js. Fully functioning

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const numKey = document.querySelectorAll('.num-key');
 const display = document.querySelector('.display-text');
 const operatorKey = document.querySelectorAll('.operator-key');
 const evalText = document.querySelector('.eval-text');
+const acBtn = document.querySelector('.clear-key');
+const delBtn = document.querySelector('.del-key');
+const dotBtn = document.querySelector('.dot-key');
+
 
 const regex = /[+\-*/]/;
-
-
 
 
 numKey.forEach((key) => {
@@ -24,16 +26,18 @@ operatorKey.forEach((key) => {
         } else if (regex.test(evalText.textContent.split('').pop()) && display.textContent !== ''){
             evalExpression(evalText.textContent,key.value,display.textContent);
         }else {
-            evalText.textContent = display.textContent + key.value;
-            display.textContent = '';
+            if (key.value !== '='){
+                evalText.textContent = display.textContent + key.value;
+                display.textContent = '';
+            }
         }
     })
 });
 
 
 function evalExpression(pre, operator, post){
-    const evaluated = math.evaluate(pre + post);
-    if (operator === '='){
+    const evaluated = Math.round(math.evaluate(pre + post) * 100) / 100;
+    if (operator === '=' && display.textContent !== ''){
         display.textContent = evaluated;
         evalText.textContent = '';
     } else {
@@ -41,3 +45,22 @@ function evalExpression(pre, operator, post){
         evalText.textContent = evaluated + operator
     }
 }
+
+acBtn.addEventListener('click', () => {
+    display.textContent = '0';
+    evalText.textContent = '';
+});
+
+delBtn.addEventListener('click', () => {
+    if (display.textContent !== ''){
+        display.textContent = display.textContent.substring(0, display.textContent.length - 1);
+    } else if (display.textContent === '' && evalText.textContent !== ''){
+        display.textContent = evalText.textContent.substring(0, evalText.textContent.length - 1);
+        evalText.textContent = '';
+ }});
+
+dotBtn.addEventListener('click', () => {
+    if (!display.textContent.includes('.')){
+        display.textContent += '.';
+    }
+});
